faeb72acf1dde2ed7d33bb7dd66fb5237c6fb712
Kunniii <nguyenairport56@gmail.com>
2023-08-09 22:00:26 +0700

    EMEMOKEDSE

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ import json
 import pickle
 import hashlib
 import logging
+import datetime
 
 logging.basicConfig(
-    level=logging.WARNING,
+    level=logging.INFO,
     filename="work.log",
     format="%(levelname)s - - %(asctime)s - %(message)s",
 )
 time_start = time.time()
+available_drives = []
+file_paths = []
+
+max_last_modified_time = 3600
+
 
 logging.info("Starting job")
 
-available_drives = []
-file_paths = []
+
+# for simplicity, only scan user's home
+user_home = os.path.expanduser("~")
 
 
 def get_available_drives():
@@ -27,7 +34,7 @@ def get_available_drives():
 
 
 def hash_file(file_path, algorithm="sha256"):
-    logging.info(f"Hashing {file_path}")
+    # logging.info(f"Hashing {file_path}")
     hash_obj = hashlib.new(algorithm)
     try:
         with open(file_path, "rb") as f:
@@ -45,26 +52,52 @@ else:
 
 logging.info("Discovering all files")
 
-for drive in available_drives:
-    for root, sub_folder, files in os.walk(drive):
-        if "$" in root:
-            continue
+# for drive in available_drives:
+#     for root, sub_folder, files in os.walk(drive):
+#         if "$" in root:
+#             continue
+#         for file_name in files:
+#             path = os.path.join(root, file_name)
+#             file_paths.append(path)
+
+try:
+    logging.info("Trying to load from pickle")
+
+    timestamp = os.path.getmtime("file_paths.pickle")
+    last_modified = datetime.datetime.fromtimestamp(timestamp)
+
+    current_time = datetime.datetime.now()
+    time_difference = current_time - last_modified
+
+    if time_difference.total_seconds() <= max_last_modified_time:
+        raise Exception(f"Last modified time exceeded {max_last_modified_time}s")
+
+    with open("file_paths.pickle", "rb") as f:
+        file_paths = pickle.load(f)
+    logging.info("Load from pickle")
+except Exception as e:
+    logging.warning(str(e))
+    for root, sub_folder, files in os.walk(user_home):
         for file_name in files:
             path = os.path.join(root, file_name)
+            print(path)
             file_paths.append(path)
+
 
 logging.info(f"Found {len(file_paths)} files")
 
 path_with_hash = []
 
+with open("file_paths.pickle", "wb+") as f:
+    pickle.dump(file_paths, file=f)
+
 for path in file_paths:
     c_index = file_paths.index(path) + 1
-    file_hash = hash_file(path)
+    file_hash = hash_file(path, "sha1")
+    print(f"Hashing [ {file_paths.index(path)}/{len(file_paths)} ]")
     current_object = {"path": path, "hash": file_hash}
     path_with_hash.append(current_object)
 
-with open("file_paths.pickle", "wb+") as f:
-    pickle.dump(file_paths, file=f)
 
 with open("path_with_hash.pickle", "wb+") as f:
     pickle.dump(path_with_hash, file=f)
@@ -72,4 +105,4 @@ with open("path_with_hash.pickle", "wb+") as f:
 with open("output.json", "w+", encoding="utf-8") as f:
     print(json.dumps(path_with_hash, indent=2, ensure_ascii=True), file=f)
 
-logging.info("Completed in {(time.time() - time_start):.2f} ms")
+logging.info(f"Completed in {(time.time() - time_start):.2f} ms")
